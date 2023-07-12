178fc8bddeb01c646276bd16f9e74bd279a9f10e
I513945 <akshay.iyyadurai.balasundaram@sap.com>
2023-07-12 13:52:54 -0500

    adds cars

--- apis/cars/v1alpha1/groupversion_info.go
+++ apis/cars/v1alpha1/groupversion_info.go
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1alpha1 contains the v1alpha1 group Sample resources of the Template provider.
+// Package v1alpha1 contains the v1alpha1 group Sample resources of the Playground provider.
 // +kubebuilder:object:generate=true
-// +groupName=sample.template.crossplane.io
+// +groupName=cars.playground.crossplane.io
 // +versionName=v1alpha1
 package v1alpha1
 
@@ -27,7 +27,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "sample.template.crossplane.io"
+	Group   = "cars.playground.crossplane.io"
 	Version = "v1alpha1"
 )
 
